2b9e329276eaf7ea6618497046073274270cb073
hc-latam <janaravena@yahoo.com>
2022-03-03 21:03:49 -0300

    bugs rutas img eventos

--- js/listar-eventos.js
+++ js/listar-eventos.js
@@ -37,24 +37,24 @@ function obtenerListadoEventos(){
 function crearTarjetaEvento(nombre, logo, fecha, artefacto, joyeria, armaduras, armas, outfit) {
 
     var contenido = "<div class='col-xl-3 col-sm-6 mb-5'><div class='bg-dark rounded shadow-sm py-5 px-4'>" +
-    "<img src='" + logo + "' alt='" + nombre + "' width='150' class='img-fluid rounded-circle mb-3 img-thumbnail shadow-sm'>"+
+    "<img src='img/eventos/" + logo + "' alt='" + nombre + "' width='150' class='img-fluid rounded-circle mb-3 img-thumbnail shadow-sm'>"+
     "<h5 class='mb-0'>" + nombre + "</h5><span class='small text-uppercase text-muted'>" + fecha + "</span>"+
-    "<ul class='list-group list-group-flush mb-0 mt-3'>";
+    "<ul class='list-group list-group-flush bg-dark mb-0 mt-3'>";
 
     if (artefacto) {
         contenido += "<li class='list-group-item'><img src='img/eventos/artefactos/" + artefacto + "' alt='" + artefacto + "' height='60' class='img-fluid mb-3 img-thumbnail shadow-sm'></li>";
     }
     if (joyeria) {
-        contenido += "<li class='list-group-item'><img src='img/eventos/artefactos/" + joyeria + "' alt='" + joyeria + "' height='60' class='img-fluid mb-3 img-thumbnail shadow-sm'></li>";
+        contenido += "<li class='list-group-item'><img src='img/eventos/joyeria/" + joyeria + "' alt='" + joyeria + "' height='60' class='img-fluid mb-3 img-thumbnail shadow-sm'></li>";
     }
     if (armaduras) {
-        contenido += "<li class='list-group-item'><img src='img/eventos/artefactos/" + armaduras + "' alt='" + armaduras + "' height='60' class='img-fluid mb-3 img-thumbnail shadow-sm'></li>";
+        contenido += "<li class='list-group-item'><img src='img/eventos/armaduras/" + armaduras + "' alt='" + armaduras + "' height='60' class='img-fluid mb-3 img-thumbnail shadow-sm'></li>";
     }
     if (armas) {
-        contenido += "<li class='list-group-item'><img src='img/eventos/artefactos/" + armas + "' alt='" + armas + "' height='60' class='img-fluid mb-3 img-thumbnail shadow-sm'></li>";
+        contenido += "<li class='list-group-item'><img src='img/eventos/armas/" + armas + "' alt='" + armas + "' height='60' class='img-fluid mb-3 img-thumbnail shadow-sm'></li>";
     }
     if (outfit) {
-        contenido += "<li class='list-group-item'><img src='img/eventos/artefactos/" + outfit + "' alt='" + outfit + "' height='60' class='img-fluid mb-3 img-thumbnail shadow-sm'></li>";
+        contenido += "<li class='list-group-item'><img src='img/eventos/outfit/" + outfit + "' alt='" + outfit + "' height='60' class='img-fluid mb-3 img-thumbnail shadow-sm'></li>";
     }
     
     contenido += "</ul></div></div>";
