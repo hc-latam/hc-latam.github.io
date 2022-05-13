13d83b196ac589b6b098abe3cec82d96c007f05b
hc-latam <janaravena@yahoo.com>
2022-05-12 20:34:45 -0400

    mejora detalle artefacto

--- js/detalle-artefacto.js
+++ js/detalle-artefacto.js
@@ -9,21 +9,19 @@ $('#artefactoModal').on('show.bs.modal', function (event) {
             if (artefacto == 0) {
                 $('#artefactoModalLabel').text('No disponible');
                 $('#artefactoModal p.descripcion').text('Pronto agregaremos la descripción.');
-                $('#artefactoModal p.activa').hide();
-                $('#artefactoModal p.subtitulo').hide();
-                $('#artefactoModal p.tiempo').hide();
                 $('#artefactoModal .modal-header img').attr('src', 'img/general/pronto.png').attr('alt', 'Pronto...');
 
+                $('#artefactoModalLabel').fadeIn();
+                $('#artefactoModal p.descripcion').fadeIn();
+                $('#artefactoModal .modal-header img').fadeIn();
+
             } else {
-                $('#artefactoModal p.activa').show();
-                $('#artefactoModal p.subtitulo').show();
-                $('#artefactoModal p.tiempo').show();
+
 
                 $('#artefactoModalLabel').text(artefacto.nombre);
                 $('#artefactoModal p.descripcion').text(artefacto.descripcion);
                 $('#artefactoModal p.activa span').text(artefacto.se_activa);
                 $('#artefactoModal p.subtitulo').text(artefacto.sub_titulo);
-                $('#artefactoModal p.tiempo span').text(artefacto.tiempo);
                 $('#artefactoModal .modal-header img').attr('src', 'img/eventos/artefactos/' + artefacto.imagen).attr('alt', 'img/eventos/artefactos/' + artefacto.nombre);
 
                 let elementToFindDigitsIn = document.querySelector('p.descripcion');
@@ -31,6 +29,17 @@ $('#artefactoModal').on('show.bs.modal', function (event) {
                     elementToFindDigitsIn
                         .textContent
                         .replace(/(\$?\d+)/g, '<span>$1</span>').replace(/(\$?\%+)/g, '<span>$1</span>');
+
+                $('#artefactoModalLabel').fadeIn();
+                $('#artefactoModal p.descripcion').fadeIn();
+                $('#artefactoModal p.activa').fadeIn();
+                $('#artefactoModal p.subtitulo').fadeIn();
+                $('#artefactoModal .modal-header img').fadeIn();
+
+                if (artefacto.tiempo) {
+                    $('#artefactoModal p.tiempo span').text(artefacto.tiempo);
+                    $('#artefactoModal p.tiempo').fadeIn();
+                }
             }
         },
         error: function (error) {
@@ -39,5 +48,13 @@ $('#artefactoModal').on('show.bs.modal', function (event) {
         }
     })
 
-})
+});
 
+$('#artefactoModal').on('hidden.bs.modal', function (event) {
+    $('#artefactoModalLabel').hide();
+    $('#artefactoModal p.descripcion').hide();
+    $('#artefactoModal p.activa').hide();
+    $('#artefactoModal p.subtitulo').hide();
+    $('#artefactoModal p.tiempo').hide();
+    $('#artefactoModal .modal-header img').hide();
+});
