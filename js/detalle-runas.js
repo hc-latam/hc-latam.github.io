f9c87046350a2a9d7807b0a064a159b6b326db36
hc-latam <janaravena@yahoo.com>
2022-06-06 19:15:36 -0400

    ajustes runas

--- js/detalle-runas.js
+++ js/detalle-runas.js
@@ -25,9 +25,9 @@ $('#runasModal').on('show.bs.modal', function (event) {
                 $('#runasModal #masacre').show();
                 $('#runasModal #diversion').show();
 
-                $('#runasModal #conflicto .modal-header img').attr('src', 'img/eventos/runas/' + runa.conflicto_imagen + '.png').attr('alt', runa.conflicto_nombre);
-                $('#runasModal #masacre .modal-header img').attr('src', 'img/eventos/runas/' + runa.masacre_imagen + '.png').attr('alt', runa.masacre_nombre);
-                $('#runasModal #diversion .modal-header img').attr('src', 'img/eventos/runas/' + runa.diversion_imagen + '.png').attr('alt', runa.diversion_nombre);
+                $('#runasModal #conflicto .modal-header img').attr('src', 'img/eventos/runas/' + runa.base_imagen + '-runa-conflicto.png').attr('alt', runa.conflicto_nombre);
+                $('#runasModal #masacre .modal-header img').attr('src', 'img/eventos/runas/' + runa.base_imagen + '-runa-masacre.png').attr('alt', runa.masacre_nombre);
+                $('#runasModal #diversion .modal-header img').attr('src', 'img/eventos/runas/' + runa.base_imagen + '-runa-diversion.png').attr('alt', runa.diversion_nombre);
 
                 $('#runasModal #conflicto .modal-title').text('Runa ' + runa.conflicto_nombre);
                 $('#runasModal #conflicto .modal-title').fadeIn();
@@ -36,12 +36,34 @@ $('#runasModal').on('show.bs.modal', function (event) {
                 $('#runasModal #diversion .modal-title').text('Runa ' + runa.diversion_nombre);
                 $('#runasModal #diversion .modal-title').fadeIn();
 
-                $('#runasModal .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar);
-                $('#runasModal .modal-body .mejorar').show();
+                if (runa.difieren === "1") {
+                    $('#runasModal #conflicto .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar + 'de ' + runa.conflicto_tipo.toLowerCase() + 's');
+                    $('#runasModal #masacre .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar + 'de ' + runa.masacre_tipo.toLowerCase() + 's');
+                    $('#runasModal #conflicto .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar + 'de ' + runa.conflicto_tipo.toLowerCase() + 's');
 
-                $('#runasModal .modal-body .descripcion').html('Usa la runa con un ' + runa.descripcion_usa_con +
-                    ' para <span>' + runa.descripcion_destacada + '</span> ' + runa.descripcion_restante + '<br /> ' +
-                    'Puedes retirar la runa en cualquier momento y usarla en otra ' + runa.descripcion_usa_con + '.');
+                    let elementoTanqueMejorar = document.querySelector('#runasModal .modal-body .mejorar');
+                    elementoTanqueMejorar.innerHTML = elementoTanqueMejorar.textContent.replace('tanques', 'guerreros');
+
+                    $('#runasModal #conflicto .modal-body .descripcion').html('Usa la runa con un ' + runa.descripcion_usa_con +
+                        ' para <span>' + runa.descripcion_destacada.replace("REMPLAZAR", runa.conflicto_tipo.toUpperCase()) + '</span> ' + runa.descripcion_restante + '<br /> ' +
+                        'Puedes retirar la runa en cualquier momento y usarla en otra ' + runa.descripcion_usa_con + '.');
+                    $('#runasModal #masacre .modal-body .descripcion').html('Usa la runa con un ' + runa.descripcion_usa_con +
+                        ' para <span>' + runa.descripcion_destacada.replace("REMPLAZAR", runa.masacre_tipo.toUpperCase()) + '</span> ' + runa.descripcion_restante + '<br /> ' +
+                        'Puedes retirar la runa en cualquier momento y usarla en otra ' + runa.descripcion_usa_con + '.');
+                    $('#runasModal #diversion .modal-body .descripcion').html('Usa la runa con un ' + runa.descripcion_usa_con +
+                        ' para <span>' + runa.descripcion_destacada.replace("REMPLAZAR", runa.diversion_tipo.toUpperCase()) + '</span> ' + runa.descripcion_restante + '<br /> ' +
+                        'Puedes retirar la runa en cualquier momento y usarla en otra ' + runa.descripcion_usa_con + '.');
+
+                    let elementoTanqueDescripcion = document.querySelector('#runasModal .modal-body .descripcion');
+                    elementoTanqueDescripcion.innerHTML = elementoTanqueDescripcion.textContent.replace('TANQUE', 'GUERRERO');
+                } else {
+                    $('#runasModal .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar);
+
+                    $('#runasModal .modal-body .descripcion').html('Usa la runa con un ' + runa.descripcion_usa_con +
+                        ' para <span>' + runa.descripcion_destacada + '</span> ' + runa.descripcion_restante + '<br /> ' +
+                        'Puedes retirar la runa en cualquier momento y usarla en otra ' + runa.descripcion_usa_con + '.');
+                }
+                $('#runasModal .modal-body .mejorar').show();
                 $('#runasModal .modal-body .descripcion').show();
 
                 if (runa.conflicto_tipo) {
@@ -51,24 +73,6 @@ $('#runasModal').on('show.bs.modal', function (event) {
                     $('#runasModal .modal-content .tipo').show();
                 } else {
                     $('#runasModal .modal-content .tipo').hide();
-                }
-
-                if (runa.difieren === "1") {
-                    var mejorar = $('#runasModal #conflicto .modal-body .mejorar').text();
-                    $('#runasModal #conflicto .modal-body .mejorar').text(mejorar + 'de ' + runa.conflicto_tipo + 's');
-                    $('#runasModal #masacre .modal-body .mejorar').text(mejorar + 'de ' + runa.masacre_tipo + 's');
-                    $('#runasModal #diversion .modal-body .mejorar').text(mejorar + 'de ' + runa.diversion_tipo + 's');
-
-                    var descripcion = $('#runasModal #conflicto .modal-body .descripcion').text();
-                    $('#runasModal #conflicto .modal-body .descripcion').text(descripcion.replace("REMPLAZAR", runa.conflicto_tipo.toUpperCase()));
-                    $('#runasModal #masacre .modal-body .descripcion').text(descripcion.replace("REMPLAZAR", runa.masacre_tipo.toUpperCase()));
-                    $('#runasModal #diversion .modal-body .descripcion').text(descripcion.replace("REMPLAZAR", runa.diversion_tipo.toUpperCase()));
-
-                    let elementoTanqueMejorar = document.querySelector('#runasModal .modal-body .mejorar');
-                    elementoTanqueMejorar.innerHTML = elementoTanqueMejorar.textContent.replace('Tanques', 'Guerreros');
-
-                    let elementoTanqueDescripcion = document.querySelector('#runasModal .modal-body .descripcion');
-                    elementoTanqueDescripcion.innerHTML = elementoTanqueDescripcion.textContent.replace('TANQUE', 'GUERRERO');
                 }
 
                 $('#runasModal .modal-header img').show();
