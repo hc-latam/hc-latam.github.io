a8470cbd5e61102d7dbf002f4289634409f8ba71
hc-latam <janaravena@yahoo.com>
2022-06-06 20:26:27 -0400

    mas ajustes

--- js/detalle-runas.js
+++ js/detalle-runas.js
@@ -36,10 +36,19 @@ $('#runasModal').on('show.bs.modal', function (event) {
                 $('#runasModal #diversion .modal-title').text('Runa ' + runa.diversion_nombre);
                 $('#runasModal #diversion .modal-title').fadeIn();
 
+                if (runa.conflicto_tipo) {
+                    $('#runasModal #conflicto.modal-content .tipo').html('<img src="img/tipos-personajes/' + runa.conflicto_tipo + '.png" alt="Tipo ' + runa.conflicto_tipo + '" />Tipo: ' + runa.conflicto_tipo);
+                    $('#runasModal #masacre.modal-content .tipo').html('<img src="img/tipos-personajes/' + runa.masacre_tipo + '.png" alt="Tipo ' + runa.masacre_tipo + '" />Tipo: ' + runa.masacre_tipo);
+                    $('#runasModal #diversion.modal-content .tipo').html('<img src="img/tipos-personajes/' + runa.diversion_tipo + '.png" alt="Tipo ' + runa.diversion_tipo + '" />Tipo: ' + runa.diversion_tipo);
+                    $('#runasModal .modal-content .tipo').show();
+                } else {
+                    $('#runasModal .modal-content .tipo').hide();
+                }
+
                 if (runa.difieren === "1") {
                     $('#runasModal #conflicto .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar + ' de ' + runa.conflicto_tipo.toLowerCase() + 's');
                     $('#runasModal #masacre .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar + ' de ' + runa.masacre_tipo.toLowerCase() + 's');
-                    $('#runasModal #diversion .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar + ' de ' + runa.conflicto_tipo.toLowerCase() + 's');
+                    $('#runasModal #diversion .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar + ' de ' + runa.diversion_tipo.toLowerCase() + 's');
 
                     $('#runasModal #conflicto .modal-body .descripcion').html('Usa la runa con un ' + runa.descripcion_usa_con +
                         ' para <span>' + runa.descripcion_destacada.replace("REMPLAZAR", runa.conflicto_tipo.toUpperCase()) + '</span> ' + runa.descripcion_restante + '<br /> ' +
@@ -50,6 +59,80 @@ $('#runasModal').on('show.bs.modal', function (event) {
                     $('#runasModal #diversion .modal-body .descripcion').html('Usa la runa con un ' + runa.descripcion_usa_con +
                         ' para <span>' + runa.descripcion_destacada.replace("REMPLAZAR", runa.diversion_tipo.toUpperCase()) + '</span> ' + runa.descripcion_restante + '<br /> ' +
                         'Puedes retirar la runa en cualquier momento y usarla en otra ' + runa.descripcion_usa_con + '.');
+                } else if (runa.difieren === "2") {
+                    var mejorasC = ''
+                    var descripcion_usa_conC = '';
+                    var tipoC = '';
+                    if (conflicto_tipo == "Hechicero") {
+                        mejorasC = 'atuendos de hechiceros';
+                        descripcion_usa_conC = 'atuendo';
+                        tipoC = 'hechicero';
+                    }
+                    if (conflicto_tipo == "Arquero") {
+                        mejorasC = 'equipamiento de arqueros';
+                        descripcion_usa_conC = 'conjunto de equipamiento';
+                        tipoC = 'arquero';
+                    }
+                    if (conflicto_tipo == "Tanque") {
+                        mejorasC = 'armadura de guerreros';
+                        descripcion_usa_conC = 'conjunto de armadura';
+                        tipoC = 'guerrero';
+                    }
+
+                    var mejorasM = ''
+                    var descripcion_usa_conM = '';
+                    var tipoM = '';
+                    if (masacre_tipo == "Hechicero") {
+                        mejorasM = 'atuendos de hechiceros';
+                        descripcion_usa_conM = 'atuendo';
+                        tipoM = 'hechicero';
+                    }
+                    if (masacre_tipo == "Arquero") {
+                        mejorasM = 'equipamiento de arqueros';
+                        descripcion_usa_conM = 'conjunto de equipamiento';
+                        tipoM = 'arquero';
+                    }
+                    if (masacre_tipo == "Tanque") {
+                        mejorasM = 'armadura de guerreros';
+                        descripcion_usa_conM = 'conjunto de armadura';
+                        tipoM = 'guerrero';
+                    }
+
+                    var mejorasD = ''
+                    var descripcion_usa_conD = '';
+                    var tipoD = '';
+                    if (diversion_tipo == "Hechicero") {
+                        mejorasD = 'atuendos de hechiceros';
+                        descripcion_usa_conD = 'atuendo';
+                        tipoD = 'hechicero';
+                    }
+                    if (diversion_tipo == "Arquero") {
+                        mejorasD = 'equipamiento de arqueros';
+                        descripcion_usa_conD = 'conjunto de equipamiento';
+                        tipoD = 'arquero';
+                    }
+                    if (diversion_tipo == "Tanque") {
+                        mejorasD = 'armadura de guerreros';
+                        descripcion_usa_conD = 'conjunto de armadura';
+                        tipoD = 'guerrero';
+                    }
+
+                    $('#runasModal #conflicto .modal-body .mejorar').text('Se usa para mejorar ' + mejorasC);
+                    $('#runasModal #masacre .modal-body .mejorar').text('Se usa para mejorar ' + mejorasM);
+                    $('#runasModal #diversion .modal-body .mejorar').text('Se usa para mejorar ' + mejorasD);
+
+                    $('#runasModal #conflicto .modal-body .descripcion').html('Usa la runa con un ' + descripcion_usa_conC +
+                    ' para <span>' + runa.descripcion_destacada.replace("REMPLAZAR", tipoC) + '</span> ' + runa.descripcion_restante + '<br /> ' +
+                    'Puedes retirar la runa en cualquier momento y usarla en otra ' + descripcion_usa_conC + '.');
+
+
+                    $('#runasModal #masacre .modal-body .descripcion').html('Usa la runa con un ' + descripcion_usa_conM +
+                        ' para <span>' + runa.descripcion_destacada.replace("REMPLAZAR", tipoM) + '</span> ' + runa.descripcion_restante + '<br /> ' +
+                        'Puedes retirar la runa en cualquier momento y usarla en otra ' + descripcion_usa_conM + '.');
+
+                    $('#runasModal #diversion .modal-body .descripcion').html('Usa la runa con un ' + descripcion_usa_conD +
+                        ' para <span>' + runa.descripcion_destacada.replace("REMPLAZAR", tipoD ) + '</span> ' + runa.descripcion_restante + '<br /> ' +
+                        'Puedes retirar la runa en cualquier momento y usarla en otra ' + descripcion_usa_conD + '.');
                 } else {
                     $('#runasModal .modal-body .mejorar').text('Se usa para mejorar ' + runa.mejorar);
 
@@ -59,15 +142,6 @@ $('#runasModal').on('show.bs.modal', function (event) {
                 }
                 $('#runasModal .modal-body .mejorar').show();
                 $('#runasModal .modal-body .descripcion').show();
-
-                if (runa.conflicto_tipo) {
-                    $('#runasModal #conflicto.modal-content .tipo').html('<img src="img/tipos-personajes/' + runa.conflicto_tipo + '.png" alt="Tipo ' + runa.conflicto_tipo + '" />Tipo: ' + runa.conflicto_tipo);
-                    $('#runasModal #masacre.modal-content .tipo').html('<img src="img/tipos-personajes/' + runa.masacre_tipo + '.png" alt="Tipo ' + runa.masacre_tipo + '" />Tipo: ' + runa.masacre_tipo);
-                    $('#runasModal #diversion.modal-content .tipo').html('<img src="img/tipos-personajes/' + runa.diversion_tipo + '.png" alt="Tipo ' + runa.diversion_tipo + '" />Tipo: ' + runa.diversion_tipo);
-                    $('#runasModal .modal-content .tipo').show();
-                } else {
-                    $('#runasModal .modal-content .tipo').hide();
-                }
 
                 $('#runasModal .modal-header img').show();
 
